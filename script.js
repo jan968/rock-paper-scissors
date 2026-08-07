ee998148be92e6a3c4a7b54dc8081d8267dbd8bd
Jan <jan968@gmail.com>
2026-08-07 18:26:03 +0200

    Add computer choice logic

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 "use strict";
+
+// 1. Computer randomly chooses rock, paper, or scissors
+function getComputerChoice() {
+   const randomNumber = Math.floor(Math.random()*3);
+
+   let computerChoice;
+   
+   if(randomNumber === 0) {
+    computerChoice = 'Rock';
+   } else if (randomNumber === 1) {
+    computerChoice = 'Paper';
+   } else {
+    computerChoice = 'Scissors';
+   }
+   return computerChoice;
+}
+
+console.log(getComputerChoice());
