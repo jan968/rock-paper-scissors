8dc5f1666b604549738f1cb8a43b8e2e92c102a9
Jan <jan968@gmail.com>
2026-08-08 11:01:50 +0200

    Add logic to play a single round

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// 1. Computer randomly chooses rock, paper, or scissors
+// 1. Computer choosing
 
 function getComputerChoice() {
    const randomNumber = Math.floor(Math.random()*3);
@@ -14,10 +14,43 @@ function getComputerChoice() {
    }
 }
 
+// 2. Get user input
 const getHumanChoice = () => prompt("Rock, paper, or scissors?");
 
 const computerChoice = getComputerChoice();
 const humanChoice = getHumanChoice();
 
-console.log(`Computer: ${computerChoice}
-Human: ${humanChoice}`);
+
+// 3. Play one round
+    function playRound (computerChoice, humanChoice) {
+
+        const computer = computerChoice.toLowerCase();
+        const human = humanChoice.toLowerCase();
+        let outcome;
+
+        
+        if(human === 'rock' && computer === 'paper') {
+            outcome = `Computer played ${computer}, you played ${human}. You lose! ${computer} beats ${human}!`;
+        } else if (human === 'rock' && computer === 'scissors') {
+            outcome = `Computer played ${computer}, you played ${human}. You win! ${human} beats ${computer}!`;
+        } else if (human === 'rock' && computer === 'rock') {
+            outcome = `Computer played ${computer}, you played ${human}. It's a draw!`;
+        } else if (human === 'paper' && computer === 'rock' ) {
+            outcome = `Computer played ${computer}, you played ${human}. You win! ${human} beats ${computer}!`;
+        } else if (human === 'paper' && computer === 'scissors' ) {
+            outcome = `Computer played ${computer}, you played ${human}. You lose! ${computer} beats ${human}!`;
+        } else if (human === 'paper' && computer === 'paper') {
+            outcome = `Computer played ${computer}, you played ${human}. It's a draw!`;
+        } else if (human === 'scissors' && computer === 'rock' ) {
+            outcome = `Computer played ${computer}, you played ${human}. You lose! ${computer} beats ${human}!`;
+        } else if (human === 'scissors' && computer === 'paper') {
+            outcome = `Computer played ${computer}, you played ${human}. You win! ${human} beats ${computer}!`;
+        } else if (human === 'scissors' && computer === 'scissors') {
+            outcome = `Computer played ${computer}, you played ${human}. It's a draw!`;
+        }
+
+        return outcome;
+    }
+
+const result = playRound(computerChoice, humanChoice);
+console.log(result);
